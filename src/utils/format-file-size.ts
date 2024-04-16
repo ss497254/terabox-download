5d8fcc52eb156a9afbcaac7c6876e1e8833ab21b
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2024-04-17 03:19:27 +0530

    update: fix format-file-size.ts

--- src/utils/format-file-size.ts
+++ src/utils/format-file-size.ts
@@ -1,12 +1,10 @@
 export function formatFileSize(size: number) {
-  if (size < 1000) {
-    return size + "B";
-  } else if (size < 1000_000) {
-    return (size / 1000).toFixed(2) + "KB";
-  } else if (size < 1000_000_000) {
-    return (size / 1000_000).toFixed(2) + "MB";
-  } else if (size < 1000_000_000_000) {
+  if (size > 1000_000_000) {
     return (size / 1000_000_000).toFixed(2) + "GB";
+  } else if (size > 1000_000) {
+    return (size / 1000_000).toFixed(2) + "MB";
+  } else if (size > 1000) {
+    return (size / 1000).toFixed(2) + "KB";
   } else {
     return size + "B";
   }
