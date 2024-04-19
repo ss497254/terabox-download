dbb78096d586f3eb6ee5ac9a0ff845bc9c38b9f6
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2024-04-19 22:58:33 +0530

    update: video-player.ts

--- src/video-player.ts
+++ src/video-player.ts
@@ -1,136 +1,47 @@
 import { setHTML } from "./html";
-import { formatFileSize } from "./utils/format-file-size";
-import { exitFullscreen, requestFullscreen } from "./utils/fullscreen";
-
-export function setupVideoPlayer({ server_filename, dlink, thumbs, size }: any) {
-  setHTML(server_filename);
-  attachVideoPlayer({ src: dlink, poster: thumbs.url3, size });
-}
+import { getKeyboardActions } from "./keyboard-actions";
+import { formatFileSize } from "./utils";
 
 function attachVideoPlayer({ src, poster, size }: any) {
-  const root = document.getElementById("root") as HTMLDivElement;
-  const overlay = document.getElementById("overlay") as HTMLDivElement;
-  const video = document.createElement("video");
+  const wrapper = document.getElementById("wrapper") as HTMLDivElement;
+  const message = document.getElementById("message") as HTMLDivElement;
+  const video = document.createElement("video") as HTMLVideoElement;
 
   video.src = src;
   video.poster = poster;
+  video.loop = true;
 
-  root.appendChild(video);
-  overlay.innerText = formatFileSize(size);
+  wrapper.appendChild(video);
+  message.innerText = formatFileSize(size);
 
   function showMessage(str: string) {
     const self = showMessage as any;
 
-    overlay.innerText = str;
+    message.innerText = str;
 
     clearTimeout(self.timeout);
     self.timeout = setTimeout(() => {
-      overlay.innerText = "";
+      message.innerText = "";
     }, 3000);
   }
 
-  const Actions: Record<string, (e: KeyboardEvent) => void> = {
-    KeyC: function () {
-      if (video.controls) {
-        video.controls = false;
-        showMessage("Controls: off");
-      } else {
-        video.controls = true;
-        showMessage("Controls: on");
-      }
-    },
-    KeyD: function () {
-      const a = document.createElement("a");
-      a.href = src;
-      a.download = "true";
-      a.click();
-      showMessage("Downloading...");
-    },
-    KeyF: function () {
-      if (video.getAttribute("fullscreen") !== "true") {
-        requestFullscreen();
-        video.setAttribute("fullscreen", "true");
-        showMessage("Fullscreen: on");
-      } else {
-        exitFullscreen();
-        video.setAttribute("fullscreen", "false");
-        showMessage("Fullscreen: off");
-      }
-    },
-    KeyM: function () {
-      if (video.getAttribute("muted") !== "true") {
-        video.muted = true;
-        video.setAttribute("muted", "true");
-        showMessage("Mute: on");
-      } else {
-        video.muted = false;
-        video.setAttribute("muted", "false");
-        showMessage("Mute: off");
-      }
-    },
-    KeyR: function () {
-      const r = video.getAttribute("rotate");
+  const Actions = getKeyboardActions(video, src, size);
 
-      if (!r) {
-        video.style.transform = "rotate(90deg)";
-        video.setAttribute("rotate", "90deg");
-        showMessage("Rotate: 90deg");
-      } else if (r === "90deg") {
-        video.style.transform = "rotate(180deg)";
-        video.setAttribute("rotate", "180deg");
-        showMessage("Rotate: 180deg");
-      } else if (r === "180deg") {
-        video.style.transform = "rotate(270deg)";
-        video.setAttribute("rotate", "270deg");
-        showMessage("Rotate: 270deg");
-      } else if (r === "270deg") {
-        video.style.transform = "rotate(0deg)";
-        video.removeAttribute("rotate");
-        showMessage("Rotate: 0deg");
-      }
-    },
-    Space: function () {
-      if (video.paused) {
-        video.play();
-        showMessage("Play");
-      } else {
-        video.pause();
-        showMessage("Pause");
-      }
-    },
-    ArrowRight: function () {
-      video.currentTime = video.currentTime + 5;
-      showMessage("Skip: +5sec");
-    },
-    ArrowLeft: function () {
-      video.currentTime = video.currentTime - 5;
-      showMessage("Skip: -5sec");
-    },
-    ArrowUp: function () {
-      video.volume = Math.min(video.volume + 0.1, 1);
-      showMessage("Volume: +10%");
-    },
-    ArrowDown: function () {
-      video.volume = Math.max(video.volume - 0.1, 0);
-      showMessage("Volume: -10%");
-    },
-    BracketRight: function () {
-      video.style.scale = (parseFloat(video.style.scale ? video.style.scale : "1") + 0.1).toFixed(2).toString();
-      showMessage("Skip: +0.1");
-    },
-    BracketLeft: function () {
-      video.style.scale = (parseFloat(video.style.scale ? video.style.scale : "1") - 0.1).toFixed(2).toString();
-      showMessage("Skip: -0.1");
-    },
-  };
-
-  document.addEventListener("keydown", (event) => {
+  window.addEventListener("keydown", (event) => {
     try {
       const { code } = event;
 
-      if (code in Actions) Actions[code](event);
+      if (code in Actions) showMessage(Actions[code](event));
     } catch (e) {
       console.warn(e);
     }
   });
+
+  window.addEventListener("blur", () => (video.muted = true));
+  window.addEventListener("focus", () => (video.muted = false));
+}
+
+export function setupVideoPlayer({ server_filename, dlink, thumbs, size }: any) {
+  setHTML(server_filename);
+  attachVideoPlayer({ src: dlink, poster: thumbs.url3, size });
 }
